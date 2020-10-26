859051b4d7533cc9c1e30c49231ca4401f08c4dd
unknown <k-speckmann@web.de>
2020-10-26 13:45:34 +0100

    add button integriert

--- public/index.php
+++ public/index.php
@@ -9,14 +9,12 @@ setlocale(LC_TIME, "de_DE.utf8"); //Zeitumgebung Deutschland
 
 //
 function popupUmschalten(arrayIndex) 
+{
   var popup = document.getElementById("myPopup"+arrayIndex);
   popup.classList.toggle("show");
 }
 
-function springeZuAktuell()
-{
-	document.getElementById('p0').scrollIntoView();
-}
+
 
 </script>
 	<head>
@@ -32,10 +30,8 @@ function springeZuAktuell()
 			</div>
 		</div>
 
-	<footer onload="springeZuAktuell();">
-   <div id="extra">
-		 <img src="extra.png">
-   </div>
+	<footer>
+>
 	</footer>
 	</body>
 
